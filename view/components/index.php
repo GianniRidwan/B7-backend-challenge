0300ed0816ca437a092192df4c120abee0f74435
Gianni Ridwan <99036552@mydavinci.nl>
2023-02-13 14:00:30 +0100

    add list

--- view/components/index.php
+++ view/components/index.php
@@ -1,3 +1,14 @@
-<div class ="col-md-12 text-center">
+<div class="col-md-12 text-center">
 		<a class="btn text-white-50  bg-dark border" href="<?=URL?>List/addList"> Add New List</a>
 </div>
+
+<?php foreach($list as $list) { ?>
+
+<div class="mt-3 d-inline-block border rounded">
+    <h1 class="p-1 bg-danger text-capitalize text-white rounded-top" style="color: blck;">
+    &nbsp; 
+    <?php echo $list["list_name"]; ?>
+    &nbsp; 
+</h1>
+</div>
+<?php } ?>

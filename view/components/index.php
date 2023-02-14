e852876435d63a4a6ee7948f8e7354e6a34e7442
Gianni Ridwan <99036552@mydavinci.nl>
2023-02-14 10:11:24 +0100

    add task

--- view/components/index.php
+++ view/components/index.php
@@ -11,7 +11,31 @@
     <a class="p-2 btn text-success" href="<?=URL?>list/editList/<?=$list['id']?>"><i class="far fa-edit text-white"></i></a>
     <a class="p-2 btn text-success" href="<?=URL?>list/deleteList/<?=$list['id']?>"><i class="far fa-trash-alt text-white"></i></a>
     </h1>
-    <a class="btn text-white bg-dark d-block rounded-0" href="<?=URL?>Task/addTask?list_id=<?php echo $list["id"];?>">Add New Task</a>
 
+    <a class="btn text-white bg-dark d-block rounded-0" href="<?=URL?>Task/addTask?list_id=<?php echo $list["id"];?>">Add New Task</a>
+    <table class= "table-bordered" width = 100%;>
+        <thead>
+            <tr>
+                <th>Task  </th>
+                <th>Duration  </th>
+                <th>Status </th>
+                <th>Edit/Delete </th>
+            </tr>
+        </thead>
+        <tbody>
+    <?php
+        foreach($task as $taskP) {
+        if ($taskP["list_id"] == $list["id"]) {   
+    ?>
+        <tr>
+            <td> <?php echo $taskP["description"];?> </td>
+            <td> <?php echo $taskP["time"];?> </td>
+            <td> <?php echo $taskP["state"];?> </td>
+            <td><a class="btn text-primary" href="<?=URL?>Task/editTask/<?= $taskP['task_id'] ?>"><i class="far fa-edit text-danger"></i></a>  
+            <a class="btn text-primary" href="<?=URL?>Task/deleteTask/<?= $taskP['task_id'] ?>"><i class="far fa-trash-alt text-danger"></i></a></td>
+        </tr>
+        <?php } } ?>
+        </tbody>
+    </table>
 </div>
 <?php } ?>
